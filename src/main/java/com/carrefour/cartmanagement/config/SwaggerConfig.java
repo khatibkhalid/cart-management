64ceb1c3f035cb5e78ec29036eadc426db311e0f
kkhatib <khalid.khatib.inf@gmail.com>
2024-09-27 10:46:14 +0200

    bugfix: fix Springfox Type javax.servlet.http.HttpServletRequest not present

--- src/main/java/com/carrefour/cartmanagement/config/SwaggerConfig.java
+++ src/main/java/com/carrefour/cartmanagement/config/SwaggerConfig.java
@@ -2,20 +2,15 @@ package com.carrefour.cartmanagement.config;
 
 import com.carrefour.cartmanagement.controller.CartController;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration(proxyBeanMethods = false)
-@EnableSwagger2
-@Profile({"!local","!prd"})
+@Profile({"local","!prd"})
 public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public Docket api() {
